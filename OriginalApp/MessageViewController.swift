24f83ebf2fededfa32707e2827a8f79ffe217ac7
MichinsohinKishimoto <michinoshin.kishimoto@gmail.com>
2023-07-12 03:25:48 +0900

    viewcontrollerの名付けと"MessageViewController"のUIパーツを配置しました。

---
+++ OriginalApp/MessageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MessageViewController.swift
+//  OriginalApp
+//
+//  Created by 岸本迪之進 on 2023/07/12.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+//テキスト（大）
+    @IBOutlet var bigmessagelabel : UILabel!
+//テキスト（小）
+    @IBOutlet var smallmessagelabel : UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    //popさせて最初の画面に戻したい
+    @IBAction func back(){
+        //なんちゃらかんちゃら
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
