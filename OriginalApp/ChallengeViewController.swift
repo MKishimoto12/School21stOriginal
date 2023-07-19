9c25ff8d131ebddc63af337c5297cb6e704e8b4c
MichinsohinKishimoto <michinoshin.kishimoto@gmail.com>
2023-07-19 20:20:13 +0900

    条件分岐、変数管理など色々。ボタンが押された時にチェックがつく以外の機能が全て終わった。

--- OriginalApp/ChallengeViewController.swift
+++ OriginalApp/ChallengeViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class ChallengeViewController: UIViewController {
     
+    //ミッションの配列
+    let missionarray = ["がんば","もっと","がんば","やる気だせ","帰れ","すごい！！"]//元の配列
+    lazy var missiondecidearray = missionarray.shuffled()
     //ミッション（ラベル関連）
     @IBOutlet var mission0label : UILabel! //配列0番地
     @IBOutlet var mission1label : UILabel! //配列1番地
     @IBOutlet var mission2label : UILabel! //配列2番地
     
-    let missionarray : [String] = []
+    //ボタンが奇数回目に押された/偶数回めに押された
+    var isDone: [Bool] = [false, false, false]
+    
+    
+
     //missionarray.append("何か")//配列への追加
     //タイマー関連
     var timer : Timer!
@@ -25,26 +32,59 @@ class ChallengeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //timer関係
-        timercount = 9000
-        timerlabel.text = String(timercount)
+        timercount = 8999
+        timerlabel.text = "\(timercount / 3600)時間\((timercount / 60) % 60)分\(timercount % 60)秒"
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
             if self.timercount > 0 {
                 self.timercount -= 1
-                self.timerlabel.text = String(self.timercount)}
-            else {
+                self.timerlabel.text = "\(self.timercount / 3600)時間\((self.timercount / 60) % 60)分\(self.timercount % 60)秒"
+            } else {
                 self.navigationController?.popViewController(animated: true)
             }
                 
             })
-        // Do any additional setup after loading the view.
+        
+        //ミッション（ボタンの下のやつ関連）
+        mission0label.text = String(missiondecidearray[0])
+        mission1label.text = String(missiondecidearray[1])
+        mission2label.text = String(missiondecidearray[2])
+        
+        
+        
     }
     //チェックボックス（ボタン関連）
-    @IBAction func check0(){}//一番左のチェックボックス
-    @IBAction func check1(){}//真ん中のチェックボックス
-    @IBAction func check2(){}//一番右のチェックボックス
+    @IBAction func check0(){
+        isDone[0] = !isDone[0]
+        if isDone[0] == true{
+            //ボタンの見た目をチェック付きに
+        } else {
+            //ボタンの見た目をチェックなしに
+        }
+        print(isDone)
+    }//一番左のチェックボックス
+    @IBAction func check1(){
+        isDone[1] = !isDone[1]
+        if isDone[1] == true{
+            //ボタンの見た目をチェック付きに
+        } else {
+            //ボタンの見た目をチェックなしに
+        }
+        print(isDone)
+    }//真ん中のチェックボックス
+    @IBAction func check2(){
+        isDone[2] = !isDone[2]
+        if isDone[2] == true{
+            //ボタンの見た目をチェック付きに
+        } else {
+            //ボタンの見た目をチェックなしに
+        }
+        print(isDone)
+    }//一番右のチェックボックス
     //つぎへのところ、つぎへ→next
     @IBAction func next(){
         let messageViewController = self.storyboard?.instantiateViewController(withIdentifier: "message") as! MessageViewController
+        messageViewController.isDone = isDone
+        messageViewController.timercount = timercount
         self.navigationController?.pushViewController(messageViewController, animated: true)
     }
     
--- OriginalApp/MessageViewController.swift
+++ OriginalApp/MessageViewController.swift
@@ -13,10 +13,22 @@ class MessageViewController: UIViewController {
 //テキスト（小）
     @IBOutlet var smallmessagelabel : UILabel!
     
+    var isDone : [Bool]!
+    var timercount : Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if isDone[0] == true && isDone[1] == true && isDone[2] == true {
+            bigmessagelabel.text = "おめでとう！！"
+            smallmessagelabel.text = "君は本当のコミュ強！！"
+        } else if timercount != 0 {
+            bigmessagelabel.text = "あきらめないで！！"
+            smallmessagelabel.text = "まだいける！！"
+        } else {
+            bigmessagelabel.text = "ざんねん！！"
+            smallmessagelabel.text = "次は頑張って！！"
+        }
         // Do any additional setup after loading the view.
     }
     //popさせて最初の画面に戻したい
@@ -28,14 +40,5 @@ class MessageViewController: UIViewController {
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
