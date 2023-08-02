c72c9e0c12923b0bf02026a8046c72b8f5ec2d49
MichinsohinKishimoto <michinoshin.kishimoto@gmail.com>
2023-08-02 20:17:57 +0900

    8/2 開発分

--- OriginalApp/ChallengeViewController.swift
+++ OriginalApp/ChallengeViewController.swift
@@ -10,18 +10,34 @@ import UIKit
 class ChallengeViewController: UIViewController {
     
     //ミッションの配列
-    let missionarray = ["がんば","もっと","がんば","やる気だせ","帰れ","すごい！！"]//元の配列
+    let missionarray = ["隣の席の子に話しかけよう","メンターにマイブームを聞こう","隣の子に今日の出来事を話そう","メンターに生活の愚痴を話そう","３人に名前を聞こう","5人に名前を聞こう"]//元の配列
     lazy var missiondecidearray = missionarray.shuffled()
     //ミッション（ラベル関連）
     @IBOutlet var mission0label : UILabel! //配列0番地
     @IBOutlet var mission1label : UILabel! //配列1番地
     @IBOutlet var mission2label : UILabel! //配列2番地
     
+    @IBOutlet var button0: UIButton!//これこの後一旦保留
+    
+    //これから作る、ボタンの代わりにImageViewにタップ判定をつける
+    @IBOutlet weak var button00 : UIImageView!
+    @IBOutlet weak var button01 : UIImageView!
+    @IBOutlet weak var button02 : UIImageView!
+    
+    //タップした時の関数
+    //@objc func imageViewTapped(_ sender: UITapGestureRecognizer) {
+    //    print("タップ")
+    //}
+    
+    
+    
     //ボタンが奇数回目に押された/偶数回めに押された
     var isDone: [Bool] = [false, false, false]
+    //ボタン押下時のボタンの画像
+    var image = UIImage(named: "checkmark")
     
+    let unfinishedimage = UIImage(named: "unfinishedcheckmark")
     
-
     //missionarray.append("何か")//配列への追加
     //タイマー関連
     var timer : Timer!
@@ -31,6 +47,11 @@ class ChallengeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //タップ判定の取得
+        button00.isUserInteractionEnabled = true
+        button00.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_:))))
+        
         //timer関係
         timercount = 8999
         timerlabel.text = "\(timercount / 3600)時間\((timercount / 60) % 60)分\(timercount % 60)秒"
@@ -41,23 +62,26 @@ class ChallengeViewController: UIViewController {
             } else {
                 self.navigationController?.popViewController(animated: true)
             }
-                
-            })
+            
+        })
         
         //ミッション（ボタンの下のやつ関連）
         mission0label.text = String(missiondecidearray[0])
         mission1label.text = String(missiondecidearray[1])
         mission2label.text = String(missiondecidearray[2])
-        
-        
-        
     }
     //チェックボックス（ボタン関連）
-    @IBAction func check0(){
+    @objc func imageViewTapped(_ sender: UITapGestureRecognizer) {
         isDone[0] = !isDone[0]
-        if isDone[0] == true{
-            //ボタンの見た目をチェック付きに
+        if isDone[0] {
+            //button0.setImage(image, for: .normal)
+            // let rect:CGRect = CGRect(x:30, y:460, width:90, height:90)
+            //button0.frame = rect;
+            
         } else {
+            //button0.setImage(unfinishedimage, for: .normal)
+            //button0.setImage(nil, for: .normal)
+            //print(button0.currentImage)
             //ボタンの見た目をチェックなしに
         }
         print(isDone)
@@ -87,6 +111,4 @@ class ChallengeViewController: UIViewController {
         messageViewController.timercount = timercount
         self.navigationController?.pushViewController(messageViewController, animated: true)
     }
-    
-    
 }
